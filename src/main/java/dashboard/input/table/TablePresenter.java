718a4fee88ebee47c0a0e40e2b88a829ee0c4489
stormkirill98 <stormkirill98@gmail.com>
2019-04-20 10:46:37 +0300

    fill table

--- src/main/java/dashboard/input/table/TablePresenter.java
+++ src/main/java/dashboard/input/table/TablePresenter.java
@@ -1,36 +1,28 @@
 package dashboard.input.table;
 
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.AccessibleAttribute;
 import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Font;
 import logic.Equation;
 import logic.LinearSystem;
 
 import javax.inject.Inject;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 @SuppressWarnings("Duplicates")
 public class TablePresenter implements Initializable {
-  final private int columnWidth = 50;
-  final private int rowHeight = 35;
+  final private int cellWidth = 50;
+  final private int cellHeight = 35;
 
   public AnchorPane pane;
 
@@ -39,15 +31,11 @@ public class TablePresenter implements Initializable {
 
   private int n;
   private int m;
-  private TableView<Equation> table;
 
-  private GridPane coefs;
+  private GridPane table;
 
 
   private LinearSystem system;
-
-  private boolean pressCtrl = false;
-
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
@@ -55,41 +43,97 @@ public class TablePresenter implements Initializable {
     m = dimension.get(1);
 
     system = new LinearSystem();
+    for (int i = 0; i < n; i++) {
+      Equation equation = new Equation();
+      Random r = new Random();
+      for (int j = 0; j < m + 1; j++) {
+        int rangeMin = -5;
+        int rangeMax = 5;
+        double randomValue = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+        equation.add(randomValue);
+      }
+      system.add(equation);
+    }
+
 
     createTable();
+    fillTable(system);
   }
 
   private void createTable() {
-    coefs = new GridPane();
+    table = new GridPane();
 
-    coefs.setHgap(3);
-    coefs.setVgap(3);
+    table.setHgap(n);
+    table.setVgap(m);
     ColumnConstraints constraints = new ColumnConstraints();
     constraints.setHalignment(HPos.CENTER);
-    coefs.getColumnConstraints().addAll(constraints, constraints);
-    
+    table.getColumnConstraints().addAll(constraints, constraints);
+
     for (int i = 0; i < n + 1; i++) {
       for (int j = 0; j < m + 1; j++) {
         if (i == 0) {
-          if (j == n) {
-            continue;
-          }
+          String columnName = j == m ? "" : "x" + (j + 1);
 
-          Label label = new Label("x" + (j + 1));
-          coefs.add(label, j, i);
+          Label label = new Label(columnName);
+
+          label.setAlignment(Pos.CENTER);
+          label.setPrefWidth(cellWidth);
+          label.setPrefHeight(cellHeight);
+
+          table.add(label, j, i);
           continue;
         }
 
         TextField textField = new TextField();
+
         textField.setPadding(new Insets(5));
-        textField.setPrefWidth(columnWidth);
-        textField.setPrefHeight(rowHeight);
-        coefs.add(textField, j, i);
+        textField.setPrefWidth(cellWidth);
+        textField.setPrefHeight(cellHeight);
+
+        table.add(textField, j, i);
       }
     }
 
-    pane.getChildren().add(coefs);
+    pane.getChildren().add(table);
   }
 
+  public void fillTable(LinearSystem system){
+    ObservableList<Node> cells = table.getChildren();
+    if (cells == null) {
+      return;
+    }
 
+    try {
+      for (int i = 1; i < n + 1; i++) {
+        Equation equation = system.getEquation(i - 1);
+        for (int j = 0; j < m + 1; j++) {
+          Double value = equation.get(j);
+          TextField cell;
+          try {
+            cell = (TextField) cells.get(i * (m + 1) + j);
+          } catch (ClassCastException e) {
+            System.out.println("TablePresenter fillTable " + e);
+            continue;
+          }
+
+          cell.setText(String.format("%.2f", value));
+        }
+      }
+    } catch (IndexOutOfBoundsException e){
+      e.printStackTrace();
+    }
+  }
+
+  public Node getNodeByRowColumnIndex (int row, int column) {
+    ObservableList<Node> childrens = table.getChildren();
+
+    for (Node node : childrens) {
+      if(GridPane.getRowIndex(node) == row
+              && GridPane.getColumnIndex(node) == column) {
+        return node;
+      }
+    }
+
+    return null;
+  }
 }
